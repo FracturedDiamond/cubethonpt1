db7a1853cc314c650b1e4e803d6fa50c9aa0c040
FracturedDiamond <49692399+FracturedDiamond@users.noreply.github.com>
2020-01-12 21:03:05 -0700

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Declaring the RigidBody variable 'rb'
+    public Rigidbody rb;
+
+    // Variable for amount of forward force
+    public float forwardForce = 1000f;
+    public float sidewaysForce = 500f;
+
+
+    // Using FixedUpdate() because we are using physics
+    void FixedUpdate()
+    {
+        // Add forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+
+        // Check if user is pressing the "right" key
+        if ( Input.GetKey("d") )
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        // Check if user is pressing the "left" key
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
